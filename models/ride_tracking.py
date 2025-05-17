afb86927febb2289435e69c8e76da87c35b38e37
zeddlyf <zedd.mee00@gmail.com>
2025-05-17 14:15:30 +0800

    Update ride_tracking.py

--- models/ride_tracking.py
+++ models/ride_tracking.py
@@ -1,14 +1,14 @@
-from connection import db
+# from connection import db
 
-class RideTracking:
-    def __init__(self, booking_booking_id, latitude, longitude, last_updated):
-        self.booking_booking_id = booking_booking_id  # Reference to Booking ID
-        self.latitude = latitude
-        self.longitude = longitude
-        self.last_updated = last_updated  # Timestamp of last GPS update
+# class RideTracking:
+#     def __init__(self, booking_booking_id, latitude, longitude, last_updated):
+#         self.booking_booking_id = booking_booking_id  # Reference to Booking ID
+#         self.latitude = latitude
+#         self.longitude = longitude
+#         self.last_updated = last_updated  # Timestamp of last GPS update
 
-    def to_dict(self):
-        return self.__dict__
+#     def to_dict(self):
+#         return self.__dict__
 
-# MongoDB Collection
-ride_tracking_collection = db["ride_tracking"]
+# # MongoDB Collection
+# ride_tracking_collection = db["ride_tracking"]
