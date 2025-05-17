b9f525a62dbfaccd69a7e9800107e43cc302df4b
zeddlyf <zedd.mee00@gmail.com>
2025-05-17 14:58:45 +0800

    Latest Updates

--- main.py
+++ main.py
@@ -54,4 +54,4 @@ def connect_db():
 
 # Start WebSocket server
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(host="0.0.0.0", port=5000, debug=True)
